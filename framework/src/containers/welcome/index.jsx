4d4f6311b34503483da0b1cc9fa4456c97e0fe92
pgxing <pgxing1101@163.com>
2021-12-18 17:42:26 +0800

    添加懒加载

--- framework/src/containers/welcome/index.jsx
+++ framework/src/containers/welcome/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button } from 'antd';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link, Route, Switch } from 'react-router-dom';
+//路由懒加载
+const Lazy = React.lazy(() => import('./../../components/Lazy'));
 export default function Welcome() {
     const [userName, setUserName] = useState('')
     const dispatch = useDispatch()
@@ -14,13 +16,13 @@ export default function Welcome() {
             type: 'GET_USER_DATA',
             payload: sessionStorage.getItem('account')
         })
-        
+
     }, [])
     //当userData.userName变化时候，就会触发，更新state
-    useEffect(()=>{
-        console.log('设置props userName到state',userData.userName)
+    useEffect(() => {
+        console.log('设置props userName到state', userData.userName)
         setUserName(userData.userName)
-    },[userData.userName])
+    }, [userData.userName])
     const quit = () => {
         dispatch({
             type: 'SAVE_USER_DATA',
@@ -34,6 +36,18 @@ export default function Welcome() {
     }
     return <div>
         <div>欢迎{userName}</div>
+        <div>
+            <Link to='/welcome'>首页</Link>|
+            <Link to='/welcome/lazy'>懒加载路由</Link>
+        </div>
+        <React.Suspense fallback={<div>loading...</div>}>
+            <Switch>
+                <Route exact path="/welcome" render={() => {
+                    return <div>欢迎{userName}</div>
+                }} />
+                <Route path="/welcome/lazy" component={Lazy} />
+            </Switch>
+        </React.Suspense>
         <Button onClick={quit}>退出</Button>
         <Button onClick={change}>修改</Button>
     </div>
